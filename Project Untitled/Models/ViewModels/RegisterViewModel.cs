61119afd6c634bc2ec8d3867161e3204887a9357
Marcus Eklund <marckus@gmail.com>
2019-03-21 17:23:45 +0100

    User Account Handling which includes Registering a new User, Login, Logout, Forgot Password.
    Including sending a Confirmation Email for new account and Confirmation Email for Forgot Password.

---
+++ Project Untitled/Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project_Untitled.Models.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Display(Name = "Username")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "The username is required")]
+        public string Name { get; set; }
+
+        [Display(Name = "Password")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Password is required")]
+        [UIHint("password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirm Password")]
+        [Compare(otherProperty: "Password", ErrorMessage = "Password & Confirm Password does not match!")]
+        [UIHint("password")]
+        public string ConfirmPassword { get; set; }
+
+        [Display(Name = "Email")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email address is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        public string ReturnUrl { get; set; } = "/";
+
+    }
+}
--- Project Untitled/Startup.cs
+++ Project Untitled/Startup.cs
@@ -1,18 +1,68 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity;
+using Project_Untitled.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Project_Untitled.Service;
 
 namespace Project_Untitled
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
+        public IConfiguration Configuration { get; }
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddDbContext<AppIdentityDbContext>(options => options.UseSqlServer(Configuration["Data:ProjectUntitledAccounts:ConnectionString"]));
+            services.AddIdentity<IdentityUser, IdentityRole>(options =>
+            {
+                // Password Requirements
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+
+                // Lockout Settings
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
+                options.Lockout.AllowedForNewUsers = true;
+                options.Lockout.MaxFailedAccessAttempts = 5;
+
+                // User Requirements
+                options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = true;
+                options.SignIn.RequireConfirmedEmail = true;
+
+            }).AddEntityFrameworkStores<AppIdentityDbContext>().AddDefaultTokenProviders();
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+                options.AddPolicy("RequireModeratorRole", policy => policy.RequireRole("Admin", "Moderator"));
+                options.AddPolicy("RequireMemberRole", policy => policy.RequireRole("Admin", "Moderator", "Member"));
+            });
+
+            services.AddTransient<IEmailSender, EmailSender>();
+            services.Configure<AuthMessageSenderOptions>(Configuration);
+
             services.AddMemoryCache();
             services.AddSession();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -21,12 +71,19 @@ namespace Project_Untitled
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseStatusCodePages();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
             }
 
-            app.UseStatusCodePages();
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSession();
-            app.UseMvc();
+            app.UseAuthentication();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
