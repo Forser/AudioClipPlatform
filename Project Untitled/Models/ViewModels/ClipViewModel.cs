243485d466b23c1b985c7f95f5aafd1518727c8e
Marcus Eklund <marckus@gmail.com>
2019-04-02 12:33:08 +0200

    Replace Audio Player with Plyr.IO. All major features are done! Only design and clean up to do!

---
+++ Project Untitled/Models/ViewModels/ClipViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjectUntitled.Models.ViewModels
+{
+    public class ClipViewModel
+    {
+        public string FileName { get; }
+        public string Title { get; }
+        public string ContentCreator { get; }
+
+        public string ContentPlatform { get; }
+    }
+}
