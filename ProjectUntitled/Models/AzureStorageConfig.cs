4656a40d79793d4987cd3ab1aa2287719e32949b
Marcus Eklund <marckus@gmail.com>
2019-04-04 16:30:08 +0200

    Azure Storage Blob Setup

---
+++ ProjectUntitled/Models/AzureStorageConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjectUntitled.Models
+{
+    public class AzureStorageConfig
+    {
+        public string AccountName { get; set; }
+        public string AccountKey { get; set; }
+        public string QueueName { get; set; }
+        public string ClipsContainer { get; set; }
+
+        public string StorageConnectionString { get; set; }
+    }
+}
--- ProjectUntitled/Models/ClipsRepository.cs
+++ ProjectUntitled/Models/ClipsRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using ProjectUntitled.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
